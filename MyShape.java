752df5ba7b965a731ed643d24c80f0c4350242c5
ankit <ankit.2428cseai2305@kiet.edu>
2025-08-27 09:34:03 +0530

    inheritance 27/08/2025

---
+++ MyShape.java
@@ -0,0 +1,8 @@
+public class MyShape {
+    public static void main(String[] args) {
+        Shape s=new Shape();
+        s.drow(12);
+        s.drow(12,13);
+    }
+
+}
---
+++ Shape.java
@@ -0,0 +1,12 @@
+public class Shape {
+    public void drow(double l,double b)
+    {
+        double area=l*b;
+        System.out.println("Area Rectangle "+area);
+    }
+    public void drow(double l)
+        {
+        double area=l*l;
+        System.out.println("Area Square "+area);
+        }
+}
